890d0509fe4db55f9523e559469ac9768185b325
Yuri Bernardo <yuribernardosie@gmail.com>
2025-05-04 17:23:06 -0300

    Tarefa realizada.

--- encoder.v
+++ encoder.v
@@ -5,8 +5,32 @@ module Encoder (
     input wire horario,
     input wire antihorario,
 
-    output reg A,
-    output reg B
+    output reg A, // horario
+    output reg B // antihorario
 );
-    
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            A <= 0;
+            B <= 0;
+        end else begin
+            if (horario && antihorario) begin
+                A <= 0;
+                B <= 0;
+            end else begin
+                if (horario) begin
+                    A <= 1;
+                    B <= 0;
+                end else begin
+                    if (antihorario) begin
+                        A <= 0;
+                        B <= 1;
+                    end else begin
+                        A <= 0;
+                        B <= 0;
+                    end
+                end
+            end
+        end
+    end
 endmodule
--- read_encoder.v
+++ read_encoder.v
@@ -7,5 +7,20 @@ module Read_Encoder (
 
     output reg [1:0] dir
 );
-    
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            dir <= 2'bZZ; // vazio
+        end else begin
+            if (A) begin
+                dir <= 2'b01;
+            end else begin
+                if (B) begin
+                    dir <= 2'b10;
+                end else begin
+                    dir <= 2'b00;
+                end
+            end
+        end
+    end
 endmodule
